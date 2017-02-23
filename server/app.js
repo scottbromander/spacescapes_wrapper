9b45eaaab21e0e7dabe8f55fb2475a9c4130191c
Scott Bromander <scott@primeacademy.io>
2017-02-22 20:26:49 -0600

    video page stubbed in

--- server/app.js
+++ server/app.js
@@ -4,7 +4,9 @@ var app = express();
 
 app.set("port", (process.env.PORT || 5000));
 
+
 app.get("/*", function(req,res){
+  console.log(req.params[0]);
   var file = req.params[0] || "index.html";
   res.sendFile(path.join(__dirname, "/public/", file));
 });
